c95f80362cf8b8395773605ffddc89eddb6f5f84
dahxxn <dahyeonhan4529@gmail.com>
2025-12-11 19:06:38 +0900

    feat(opening): implement print opening message

--- src/main/java/store/controller/ConvenienceSystem.java
+++ src/main/java/store/controller/ConvenienceSystem.java
@@ -4,14 +4,32 @@ import java.util.List;
 import store.domain.products.Products;
 import store.domain.promotions.Promotions;
 import store.util.ReadMdFile;
+import store.view.OutputView;
 
 public class ConvenienceSystem {
+    Promotions promotions;
+    Products products;
+
     public void run() {
+        initSystem();
+        process();
+    }
+
+    public void initSystem() {
         ReadMdFile reader = new ReadMdFile();
         List<String> rawPromotionContents = reader.readMdFile("promotions.md");
         List<String> rawProductsContents = reader.readMdFile("products.md");
 
-        Promotions promotions = new Promotions(rawPromotionContents);
-        Products products = new Products(rawProductsContents, promotions);
+        promotions = new Promotions(rawPromotionContents);
+        products = new Products(rawProductsContents, promotions);
+    }
+
+    public void process() {
+        boolean keepGoing = true;
+        while (keepGoing) {
+            OutputView.print("안녕하세요. W편의점입니다.");
+
+            keepGoing = false;
+        }
     }
 }
--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -10,4 +10,6 @@ public class OutputView {
     public static void println() {
         System.out.println();
     }
+
+
 }
